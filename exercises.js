4b9f92a1f538b5f90b27b35b62b7b02d119333bd
Chaz Kondo <chuckeekondo@gmail.com>
2018-04-11 21:16:05 -1000

    oops forgot to commit the first problem. now on problem 3

--- exercises.js
+++ exercises.js
@@ -14,7 +14,15 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
     Console.log the object.
 */
     
-  
+var books = {
+    title: 'The obstacle is the Way',
+    author: 'Ryan Holiday',
+    category: 'Self Help',
+    pages: 200
+}
+
+console.log(books);
+
 /*
 2.  Declare a variable named `dog` and create the following properties (key-value pairs) to the object:
    
@@ -30,7 +38,16 @@ Objects in Javascript can be compared to objects in real life. Objects has a col
  "My dog `name` is `age` year old and likes to `speak` at strangers."
 */ 
 
+var dog = {
 
+    name: 'Kinako',
+    age: 1,
+    vegeterian: false,
+    color: ['Light brown','White'],
+    speak: 'bark'
+}
+
+console.log("My dog " + dog.name + ' is ' + dog.age + ' year old and likes to ' + dog.speak + ' at strangers.');
 
 //An empty object
 

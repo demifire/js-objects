993ef4d2a183bb72768bf7fdf41cb0d34bdd86e4
Chaz Kondo <chuckeekondo@gmail.com>
2018-04-11 21:25:21 -1000

    4 seemed too easy

--- exercises.js
+++ exercises.js
@@ -88,6 +88,13 @@ console.log(kicks);
   console.log the object
 */
 
+var plainBox = {};
+
+plainBox.color = 'blue';
+plainBox.size = 17;
+plainBox.contents = [];
+
+console.log(plainBox);
 
 /*
 5. Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
